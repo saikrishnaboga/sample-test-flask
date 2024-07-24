e8f8079f53a138e79912b0b020d99c42c5ea0ee4
saikrishnaboga <91647030+saikrishnaboga@users.noreply.github.com>
2024-07-24 21:47:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, jsonify 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Hello World"
+
 @app.route('/transcribe')
 def index():
     return jsonify({'transcription': "transcription"})

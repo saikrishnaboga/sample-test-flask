c01eb46c7b660cc248e5fdeddb9d472b194b1e10
saikrishnaboga <91647030+saikrishnaboga@users.noreply.github.com>
2024-07-24 20:44:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-a
+let mediaRecorder;
+let recordedChunks = [];
+let recordings = [];
+let selectedRecording = null;
+let wavesurfer;
+
+
+function transcribe() {
+    if (selectedRecording !== "") {
+        const formData = new FormData();
+
+        formData.append('audio', selectedRecording.blob);
+        console.log(formData)
+        fetch('http://127.0.0.1:5000/transcribe', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json',
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            document.getElementById('transcript').value = data.transcription;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+}
